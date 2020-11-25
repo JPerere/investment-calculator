174bbbe0e985a2aa6895db2737cb550b49d921ee
Justin <justin.perere@gmail.com>
2020-11-25 15:37:33 -0600

    results view

--- src/App.js
+++ src/App.js
@@ -31,69 +31,75 @@ function App() {
     handleCloseReset();
   };
   return (
-    <Container>
-      <Row>
-        <Col lg="6">
-          <ProfileInfo
-            startAge={startAge}
-            startAgeChanged={(e) => setStartAge(e.target.value)}
-            endAge={endAge}
-            endAgeChanged={(e) => setEndAge(e.target.value)}
-            percent={percent}
-            percentChanged={(e) => setPercent(e.target.value)}
-          ></ProfileInfo>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Calculation
-            amountSaved={amountRothSaved}
-            amountSavedChanged={(e) => setAmountRothSaved(e.target.value)}
-            monthlySaved={monthlyRothSaved}
-            monthlySavedChanged={(e) => setMonthlyRothSaved(e.target.value)}
-            acctType={AccountType.PostTax}
-          ></Calculation>
-        </Col>
-        <Col>
-          <Calculation
-            amountSaved={amountSaved}
-            amountSavedChanged={(e) => setAmountSaved(e.target.value)}
-            monthlySaved={monthlySaved}
-            monthlySavedChanged={(e) => setMonthlySaved(e.target.value)}
-            acctType={AccountType.PreTax}
-          ></Calculation>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Button variant="secondary" onClick={handleShowReset}>
-            Reset
-          </Button>
-          <Button variant="primary" className="ml-2">
-            Calculate
-          </Button>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Results ageStart={startAge} ageEnd={endAge}></Results>
-        </Col>
-      </Row>
-      <Modal show={showConfirmReset} onHide={handleCloseReset}>
-        <Modal.Header closeButton>
-          <Modal.Title>Reset Values</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Are you sure you want to reset all form values?</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseReset}>
-            Cancel
-          </Button>
-          <Button variant="primary" onClick={resetValues}>
-            Reset Values
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </Container>
+    <>
+      <Container>
+        <Row>
+          <Col lg="6">
+            <ProfileInfo
+              startAge={startAge}
+              startAgeChanged={(e) => setStartAge(e.target.value)}
+              endAge={endAge}
+              endAgeChanged={(e) => setEndAge(e.target.value)}
+              percent={percent}
+              percentChanged={(e) => setPercent(e.target.value)}
+            ></ProfileInfo>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm="12" md="6">
+            <Calculation
+              amountSaved={amountRothSaved}
+              amountSavedChanged={(e) => setAmountRothSaved(e.target.value)}
+              monthlySaved={monthlyRothSaved}
+              monthlySavedChanged={(e) => setMonthlyRothSaved(e.target.value)}
+              acctType={AccountType.PostTax}
+            ></Calculation>
+            <Results
+              startAge={startAge}
+              endAge={endAge}
+              amountSaved={amountRothSaved}
+              monthlySaved={monthlyRothSaved}
+              interest={percent}
+            ></Results>
+          </Col>
+          <Col sm="12" md="6">
+            <Calculation
+              amountSaved={amountSaved}
+              amountSavedChanged={(e) => setAmountSaved(e.target.value)}
+              monthlySaved={monthlySaved}
+              monthlySavedChanged={(e) => setMonthlySaved(e.target.value)}
+              acctType={AccountType.PreTax}
+            ></Calculation>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button variant="secondary" onClick={handleShowReset}>
+              Reset
+            </Button>
+            <Button variant="primary" className="ml-2">
+              Calculate
+            </Button>
+          </Col>
+        </Row>
+        <Modal show={showConfirmReset} onHide={handleCloseReset}>
+          <Modal.Header closeButton>
+            <Modal.Title>Reset Values</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Are you sure you want to reset all form values?
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseReset}>
+              Cancel
+            </Button>
+            <Button variant="primary" onClick={resetValues}>
+              Reset Values
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </Container>
+    </>
   );
 }
 
--- src/Results.js
+++ src/Results.js
@@ -1,9 +1,75 @@
+import { Container, Row, Col } from "react-bootstrap";
+
 function Results(props) {
+  if (
+    props.startAge &&
+    props.endAge &&
+    props.amountSaved &&
+    props.monthlySaved &&
+    props.interest
+  ) {
+    let rows = getRows(props);
+    console.log(rows);
+    const rowElements = rows.map((row) => (
+      <ResultsRow
+        month={row.month}
+        balance={row.balance}
+        age={row.age}
+      ></ResultsRow>
+    ));
+    return rowElements;
+  } else {
+    return "";
+  }
+}
+
+function ResultsRow(props) {
   return (
-    <>
-      {props.ageStart}&nbsp;{props.ageEnd}
-    </>
+    <Row>
+      <Col lg="6">
+        {props.month}&nbsp;&nbsp;&nbsp;
+        {props.balance}&nbsp;&nbsp;&nbsp;
+        {props.age}
+      </Col>
+    </Row>
   );
+}
+
+function getRows(props) {
+  console.log("props;");
+  console.log(props);
+  let interestRate = props.interest.replace("%", "");
+  const monthlyInterestRate = parseFloat(interestRate) / 100 / 12;
+  console.log("Monthly interstRate:", monthlyInterestRate);
+  let balance = parseFloat(
+    props.amountSaved.replace("$", "").replace(",", "").replace(".", "")
+  );
+  let monthlySaved = parseFloat(
+    props.monthlySaved.replace("$", "").replace(",", "").replace(".", "")
+  );
+  console.log("balance:");
+  console.log(balance);
+
+  let ResultsRow = [];
+
+  let currentYear = new Date().getFullYear();
+  let age = 0;
+  for (age = props.startAge; age < props.endAge; age++) {
+    let month = 0;
+    for (month = 1; month <= 12; month++) {
+      balance =
+        Math.round(balance * (1 + monthlyInterestRate), 2) + monthlySaved;
+
+      ResultsRow.push({
+        month: `${month}/${currentYear}`,
+        balance: balance,
+        age: age,
+      });
+    }
+    currentYear++;
+  }
+
+  return ResultsRow;
 }
 
 export default Results;
